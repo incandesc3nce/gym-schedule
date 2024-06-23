fc04ceec486828aef17492274c0810a465c8a9ea
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-06-23 21:44:43 +0700

    feat: swap active button to a clicked one

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,31 @@ const removeChildren = (parent) => {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
-}
+};
+
+const getActiveButton = () => document.querySelector('.btn-active');
+
+const swapActiveButton = (event) => {
+  const activeButton = getActiveButton();
+  activeButton.classList.remove('btn-active');
+  activeButton.classList.add('btn-inactive');
+  
+  const clickedButton = event.target;
+  clickedButton.classList.remove('btn-inactive');
+  clickedButton.classList.add('btn-active');
+};
 
 const listenerFactory = () => {
   const scheduleHandler = (event) => {
     event.preventDefault();
+    swapActiveButton(event);
     removeChildren(document.querySelector('#content'));
     createTrainingsTable();
   };
 
   const trainersHandler = (event) => {
     event.preventDefault();
+    swapActiveButton(event);
     removeChildren(document.querySelector('#content'));
     createTrainersCards();
   };
